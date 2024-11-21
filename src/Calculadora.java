cb732eb4016a9d91ad3b2e82009cdf40b976d40a
mirianbeta <miyibeta1413@gmail.com>
2024-11-21 20:31:02 +0100

    Implementación de la ventana normal

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,5 +1,21 @@
-public class Calculadora {
+import javax.swing.JFrame;
+import java.awt.Toolkit;
+import java.awt.Dimension;
+
+public class Calculadora extends JFrame {
+
+    public Calculadora(){
+        setTitle("Calculadora de Mirian");
+        Toolkit miPantalla = Toolkit.getDefaultToolkit();
+        Dimension tamanoPantalla = miPantalla.getScreenSize();
+        int anchoPantalla = tamanoPantalla.width;
+        setSize(anchoPantalla/2,600);       
+        setLocationRelativeTo(null);
+    }
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Calculadora cal = new Calculadora();
+        cal.setVisible(true);
+        cal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
